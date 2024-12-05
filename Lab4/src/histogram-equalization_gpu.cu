5cddc3ca4f42dc41bc8a4a4a893b71d3367d0a20
sliaskonis <spirosliaskonis@gmail.com>
2024-12-05 23:15:28 +0200

    Added histogram equalization kernel (naive approach, no gpu optimization)

--- Lab4/src/histogram-equalization_gpu.cu
+++ Lab4/src/histogram-equalization_gpu.cu
@@ -5,7 +5,7 @@ extern "C" {
     #include "hist-equ.h"
 
     #define MAX_THREADS_PER_BLOCK 1024
-    #define GRID_DIM ceil(((float)img_size/MAX_THREADS_PER_BLOCK)/100)
+    #define GRID_DIM ceil(((float)img_size/MAX_THREADS_PER_BLOCK))
 
 	/****************************** Helper Functions ******************************/
 	bool checkCudaError(const char *step) {
@@ -20,43 +20,59 @@ extern "C" {
 	/****************************** Kernels ******************************/
 
     // Histogram calculation: stride implementation
-	__global__ void histogram_calc(int *hist_out, unsigned char *img_in, int img_size, int nbr_bin) {
-        __shared__ int private_hist[256];
+	// __global__ void histogram_calc(int *hist_out, unsigned char *img_in, int img_size, int nbr_bin) {
+    //     __shared__ int private_hist[256];
 
-        int i = threadIdx.x + blockIdx.x*blockDim.x,
-            stride = blockDim.x * gridDim.x;
+    //     int i = threadIdx.x + blockIdx.x*blockDim.x,
+    //         stride = blockDim.x * gridDim.x;
 
-        if (threadIdx.x < nbr_bin) {
-            private_hist[threadIdx.x] = 0;
-        }
-        __syncthreads();
+    //     if (threadIdx.x < nbr_bin) {
+    //         private_hist[threadIdx.x] = 0;
+    //     }
+    //     __syncthreads();
 		
-        while (i < img_size) {
-            atomicAdd(&(private_hist[img_in[i]]), 1);
-            i += stride;
-        }
-        __syncthreads();
+    //     while (i < img_size) {
+    //         atomicAdd(&(private_hist[img_in[i]]), 1);
+    //         i += stride;
+    //     }
+    //     __syncthreads();
         
-        if (threadIdx.x < nbr_bin) {
-            atomicAdd(&(hist_out[threadIdx.x]), private_hist[threadIdx.x]);
+    //     if (threadIdx.x < nbr_bin) {
+    //         atomicAdd(&(hist_out[threadIdx.x]), private_hist[threadIdx.x]);
+    //     }
+    // }
+
+    // Histogram calculation: naive implementation
+    __global__ void histogram_calc(int *d_hist_out, unsigned char *d_img_in, int nbr_bin) {
+        int tid = threadIdx.x + blockIdx.x*blockDim.x;
+		
+        atomicAdd(&(d_hist_out[d_img_in[tid]]), 1);
+    }
+
+    // Histogram equalization application: naive implementation
+    __global__ void histogram_equ(unsigned char *d_img_out, unsigned char *d_img_in, int *d_lut) {
+        int tid = threadIdx.x + blockIdx.x*blockDim.x;
+        
+        if (d_lut[d_img_in[tid]] > 255) {
+            d_img_out[tid] = 255;
+        }
+        else {
+            d_img_out[tid] = (unsigned char)d_lut[d_img_in[tid]];
         }
     }
 
-    // // Histogram calculation: naive implementation
-    // __global__ void histogram_calc(int *hist_out, unsigned char *img_in, int nbr_bin) {
-    //     int tid = threadIdx.x + blockIdx.x*blockDim.x;
-		
-    //     atomicAdd(&(hist_out[img_in[tid]]), 1);
-    // }
-
 	// Kernel wrapper
-    void histogram_gpu(int *hist_out, unsigned char *img_in,
+    void histogram_gpu(unsigned char *img_out, unsigned char *img_in,
                                 int img_size, int nbr_bin) {
         int padding = 0, padded_size = 0;
-		
-		unsigned char *d_img_in;
+        int i, cdf, min, d;
+        int *lut = (int *)malloc(sizeof(int)*nbr_bin);
+        int *hist_out = (int *)malloc(sizeof(int)*nbr_bin);
+
+		unsigned char *d_img_in, *d_img_out;
         int *d_hist_out;
-                            
+        int *d_lut;
+
         // Allocate device memory 
         padding = (img_size%MAX_THREADS_PER_BLOCK) ? (MAX_THREADS_PER_BLOCK - (img_size%MAX_THREADS_PER_BLOCK)) : 0;
 
@@ -73,16 +89,9 @@ extern "C" {
         dim3 block(MAX_THREADS_PER_BLOCK, 1, 1);
         dim3 grid(GRID_DIM, 1, 1);
 
-        clock_t start, end;
-
-        start = clock();
-        histogram_calc<<<grid, block>>>(d_hist_out, d_img_in, img_size, nbr_bin);
+        histogram_calc<<<grid, block>>>(d_hist_out, d_img_in, nbr_bin);
 		cudaDeviceSynchronize();
-        end = clock();
 
-        double time = (double) (end-start)/ CLOCKS_PER_SEC;
-
-        printf("Hist kernel time: %f\n", time);
 		checkCudaError("Histogram calculation");
 
 		// Copy calculated histogram back to host 
@@ -92,9 +101,41 @@ extern "C" {
 		// Padding elements are set to 0
         hist_out[0] = hist_out[0] - padding;
 		
+        // Construct the LUT by calculating the CDF
+        cdf = 0;
+        min = 0;
+        i = 0;
+        while(min == 0){
+            min = hist_out[i++];
+        }
+        d = img_size - min;
+        for(i = 0; i < nbr_bin; i ++){
+            cdf += hist_out[i];
+            lut[i] = (int)(((float)cdf - min)*255/d + 0.5);
+            if(lut[i] < 0){
+                lut[i] = 0;
+            }
+        }    
+        
+        // Device memory allocation for histogram equalization application kernel
+        cudaMalloc((void**) &d_lut,     sizeof(int)*nbr_bin);
+        cudaMalloc((void**) &d_img_out, sizeof(unsigned char)*padded_size);
+        
+        cudaMemcpy(d_lut, lut, sizeof(int)*nbr_bin, cudaMemcpyHostToDevice);
+        
+        cudaMemset(d_img_out, 0, sizeof(unsigned char)*padded_size);
+
+        // Launch kernel
+        histogram_equ<<<grid, block>>>(d_img_out, d_img_in, d_lut);
+
+        // Copy img back to host
+        cudaMemcpy(img_out, d_img_out, sizeof(unsigned char)*img_size, cudaMemcpyDeviceToHost);
+
         // Free non-wanted memory 
+        cudaFree(d_lut);
         cudaFree(d_img_in);
-		cudaFree(d_hist_out);
+        cudaFree(d_img_out);
+        cudaFree(d_hist_out);
 
         // Reset the device
         cudaDeviceReset();
