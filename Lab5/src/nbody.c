f438db6bdb0ddb40ace72771458c890f8d63118c
sliaskonis <spirosliaskonis@gmail.com>
2024-12-23 12:35:14 +0200

    Used openmp, created makefile

--- Lab5/src/nbody.c
+++ Lab5/src/nbody.c
@@ -9,28 +9,28 @@
 typedef struct { float x, y, z, vx, vy, vz; } Body;
 
 void randomizeBodies(float *data, int n) {
-  for (int i = 0; i < n; i++) {
-    data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-  }
+  	for (int i = 0; i < n; i++) {
+    	data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+  	}
 }
 
 void bodyForce(Body *p, float dt, int n) {
-  for (int i = 0; i < n; i++) {
-    float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
+	#pragma omp for schedule(dynamic)
+	for (int i = 0; i < n; i++) {
+		float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
 
-    for (int j = 0; j < n; j++) {
-      float dx = p[j].x - p[i].x;
-      float dy = p[j].y - p[i].y;
-      float dz = p[j].z - p[i].z;
-      float distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
-      float invDist = 1.0f / sqrtf(distSqr);
-      float invDist3 = invDist * invDist * invDist;
+		for (int j = 0; j < n; j++) {
+			float dx = p[j].x - p[i].x;
+			float dy = p[j].y - p[i].y;
+			float dz = p[j].z - p[i].z;
+			float distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
+			float invDist = 1.0f / sqrtf(distSqr);
+			float invDist3 = invDist * invDist * invDist;
 
-      Fx += dx * invDist3; Fy += dy * invDist3; Fz += dz * invDist3;
-    }
-
-    p[i].vx += dt*Fx; p[i].vy += dt*Fy; p[i].vz += dt*Fz;
-  }
+			Fx += dx * invDist3; Fy += dy * invDist3; Fz += dz * invDist3;
+		}
+		p[i].vx += dt*Fx; p[i].vy += dt*Fy; p[i].vz += dt*Fz;
+	}
 }
 
 int main(const int argc, const char** argv) {
@@ -45,20 +45,27 @@ int main(const int argc, const char** argv) {
   float *buf = (float*)malloc(bytes);
   Body *p = (Body*)buf;
 
+  printf("Available threads: %d\n", omp_get_max_threads());
+
   randomizeBodies(buf, 6*nBodies); // Init pos / vel data
+
 
   double totalTime = 0.0;
 
   for (int iter = 1; iter <= nIters; iter++) {
     StartTimer();
+	
+	#pragma omp parallel
+	{
+		bodyForce(p, dt, nBodies); // compute interbody forces
 
-    bodyForce(p, dt, nBodies); // compute interbody forces
-
-    for (int i = 0 ; i < nBodies; i++) { // integrate position
-      p[i].x += p[i].vx*dt;
-      p[i].y += p[i].vy*dt;
-      p[i].z += p[i].vz*dt;
-    }
+		#pragma omp for schedule(dynamic)
+		for (int i = 0 ; i < nBodies; i++) { // integrate position
+			p[i].x += p[i].vx*dt;
+			p[i].y += p[i].vy*dt;
+			p[i].z += p[i].vz*dt;
+		}
+	}
 
     const double tElapsed = GetTimer() / 1000.0;
     if (iter > 1) { // First iter is warm up
