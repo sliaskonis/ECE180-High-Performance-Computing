f0dd4d34244d4962007901e04ec5f60401716cb3
sliaskonis <spirosliaskonis@gmail.com>
2025-01-08 10:58:32 +0200

    Changed data distribution from array of structs to struct of arrays

--- Lab5/src/src_cuda/nbody.cu
+++ Lab5/src/src_cuda/nbody.cu
@@ -6,26 +6,31 @@
 #define SOFTENING 1e-9f  /* Will guard against denormals */
 #define THREADS_PER_BLOCK 1024
 
-typedef struct { float x, y, z, vx, vy, vz;} Body;
+typedef struct { float *x, *y, *z, *vx, *vy, *vz;} Body;
 
 /****************************** Helper Functions ******************************/
-bool checkCudaError(const char *step) {
+void checkCudaError(const char *step) {
 	cudaError_t err = cudaGetLastError();
 	if (err != cudaSuccess) {
 		printf("Error in %s: %s\n", step, cudaGetErrorString(err));
-		return true;
+		cudaDeviceReset();
+		exit(1);
 	}
-	return false;
 }
 
-void randomizeBodies(float *data, int n) {
+void randomizeBodies(Body *bodies, int n) {
   	for (int i = 0; i < n; i++) {
-    	data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    	bodies->x[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    	bodies->y[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    	bodies->z[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    	bodies->vx[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    	bodies->vy[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    	bodies->vz[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
   	}
 }
 
 /***************** KERNEL CODE *****************/
-__global__ void bodyForce(Body *p, float dt, int n) {
+__global__ void bodyForce(Body p, float dt, int n) {
 	int tid = threadIdx.x + blockIdx.x*blockDim.x;
 
 	float dx, dy, dz;
@@ -35,9 +40,9 @@ __global__ void bodyForce(Body *p, float dt, int n) {
 	float Fz = 0.0f;
 
 	for (int i = 0; i < n; i++) {
-		dx = p[i].x - p[tid].x;
-		dy = p[i].y - p[tid].y;
-		dz = p[i].z - p[tid].z;
+		dx = p.x[i] - p.x[tid];
+		dy = p.y[i] - p.y[tid];
+		dz = p.z[i] - p.z[tid];
 		distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
 		invDist = 1.0f / sqrtf(distSqr);
 		invDist3 = invDist * invDist * invDist;
@@ -47,9 +52,9 @@ __global__ void bodyForce(Body *p, float dt, int n) {
         Fz += dz * invDist3;
 	}
 
-    p[tid].vx += dt*Fx;
-	p[tid].vy += dt*Fy;
-	p[tid].vz += dt*Fz;
+    p.vx[tid] += dt*Fx;
+	p.vy[tid] += dt*Fy;
+	p.vz[tid] += dt*Fz;
 }
 
 int main(const int argc, const char** argv) {
@@ -60,47 +65,68 @@ int main(const int argc, const char** argv) {
   	const float dt = 0.01f; // time step
   	const int nIters = 10;  // simulation iterations
 
-  	int bytes = nBodies*sizeof(Body);
 	float totalTime = 0.0f, elapsed_time = 0.0f;
-	float *buf = (float*)malloc(bytes);
-	float *d_buf;
-	Body *d_p, *p = (Body*)buf;
+	Body bodies, d_bodies;
+
 	cudaEvent_t iter_start, iter_end;
 
 	cudaEventCreate(&iter_start);
 	cudaEventCreate(&iter_end);
 
-  	randomizeBodies(buf, 6*nBodies); // Init pos / vel data
+	/****************************** Host memory allocation ******************************/
+	int bytes = sizeof(float)*nBodies;
+	bodies.x = (float *)malloc(bytes);
+	bodies.y = (float *)malloc(bytes);
+	bodies.z = (float *)malloc(bytes);
+	bodies.vx = (float *)malloc(bytes);
+	bodies.vy = (float *)malloc(bytes);
+	bodies.vz = (float *)malloc(bytes);
+
+  	randomizeBodies(&bodies, nBodies); // Init pos / vel data
 
 	// Set geometry
 	dim3 block(THREADS_PER_BLOCK, 1, 1);
 	dim3 grid((int)(ceil(nBodies/THREADS_PER_BLOCK)), 1, 1);
 
 	/****************************** Data transfers ******************************/
-	cudaMalloc((void **) &d_buf, bytes);
-	cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
+	cudaMalloc((void **) &d_bodies.x, bytes);
+	cudaMalloc((void **) &d_bodies.y, bytes);
+	cudaMalloc((void **) &d_bodies.z, bytes);
+	cudaMalloc((void **) &d_bodies.vx, bytes);
+	cudaMalloc((void **) &d_bodies.vy, bytes);
+	cudaMalloc((void **) &d_bodies.vz, bytes);
 
-	d_p = (Body*)d_buf;
+	cudaMemcpy(d_bodies.x,  bodies.x,  bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_bodies.y,  bodies.y,  bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_bodies.z,  bodies.z,  bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_bodies.vx, bodies.vx, bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_bodies.vy, bodies.vy, bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_bodies.vz, bodies.vz, bytes, cudaMemcpyHostToDevice);
 
 	/****************************** Real Computation ******************************/
   	for (int iter = 1; iter <= nIters; iter++) {
 		cudaEventRecord(iter_start, 0);
 		
-		bodyForce<<<grid, block>>>(d_p, dt, nBodies);
+		bodyForce<<<grid, block>>>(d_bodies, dt, nBodies);
 		checkCudaError("bodyForce");
         cudaDeviceSynchronize();
 
         // Transfer data back to host in order to compute new coordinates
-	    cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
+	    cudaMemcpy(bodies.vx, d_bodies.vx, bytes, cudaMemcpyDeviceToHost);
+	    cudaMemcpy(bodies.vy, d_bodies.vy, bytes, cudaMemcpyDeviceToHost);
+	    cudaMemcpy(bodies.vz, d_bodies.vz, bytes, cudaMemcpyDeviceToHost);
+
 
         for (int i = 0 ; i < nBodies; i++) { // integrate position
-            p[i].x += p[i].vx*dt;
-            p[i].y += p[i].vy*dt;
-            p[i].z += p[i].vz*dt;
+            bodies.x[i] += bodies.vx[i]*dt;
+            bodies.y[i] += bodies.vy[i]*dt;
+            bodies.z[i] += bodies.vz[i]*dt;
         }
 
         // Tranfer new coordinates back to device for next computations
-	    cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
+		cudaMemcpy(d_bodies.x, bodies.x, bytes, cudaMemcpyHostToDevice);
+	    cudaMemcpy(d_bodies.y, bodies.y, bytes, cudaMemcpyHostToDevice);
+	    cudaMemcpy(d_bodies.z, bodies.z, bytes, cudaMemcpyHostToDevice);
 
         cudaEventRecord(iter_end, 0);
 		cudaEventSynchronize(iter_end);
@@ -113,10 +139,6 @@ int main(const int argc, const char** argv) {
   	}
 
   	float avgTime = totalTime / (float)(nIters-1);
-
-	/****************************** Data transfers ******************************/
-	cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
-	cudaFree(d_buf);
 
   	printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
 	printf("Total time: %.3f\n", totalTime);
@@ -136,17 +158,32 @@ int main(const int argc, const char** argv) {
 	}
 
 	for (int i = 0; i < nBodies; i++) {
-		fprintf(fd, "%f\n", p[i].x);
-		fprintf(fd, "%f\n", p[i].y);
-		fprintf(fd, "%f\n", p[i].z);
+		fprintf(fd, "%f\n", bodies.x[i]);
+		fprintf(fd, "%f\n", bodies.y[i]);
+		fprintf(fd, "%f\n", bodies.z[i]);
 	}
 
 	fclose(fd);
 
 	printf("Data written successfully\n");
 #endif
+
+	/****************************** Cleanup ******************************/
+	// Device
+	cudaFree(d_bodies.x);
+	cudaFree(d_bodies.y);
+	cudaFree(d_bodies.z);
+	cudaFree(d_bodies.vx);
+	cudaFree(d_bodies.vy);
+	cudaFree(d_bodies.vz);	
 	
-	free(buf);
-	
+	// Host
+	free(bodies.x);
+	free(bodies.y);
+	free(bodies.z);
+	free(bodies.vx);
+	free(bodies.vy);
+	free(bodies.vz);	
+
 	cudaDeviceReset();
 }
